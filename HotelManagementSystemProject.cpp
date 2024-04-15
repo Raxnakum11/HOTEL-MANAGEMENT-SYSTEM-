ca7744312f4455ade6d8ead54cfd771b29f8c7a5
Raxnakum11 <148608416+Raxnakum11@users.noreply.github.com>
2024-04-15 23:30:00 +0530

    Update HotelManagementSystemProject.cpp
    change in displayroom and summary report

--- HotelManagementSystemProject.cpp
+++ HotelManagementSystemProject.cpp
@@ -72,73 +72,33 @@ void Room::searchRoom(int rno)
     }
   if(found==1)
   {
-    cout<<"Room Details\n";
+    cout<<"Room Details :\n";
     if(rooms[i].status==1)
     {
-      cout<<\n Room is reserved";
+      cout<<\n Room is reserved.";
     }
     else
     {
-      cout<<"\n Room is available";
+      cout<<"\n Room is available.";
+      cout<<"\n-------------------------------------------------";
     }
     displayRoom(rooms[i]);
     getch();
   }
   else
   {
-    cout<<"\n Room not found";
+    cout<<"\n Room not found!!";
     getch();
   }
 }
-
 void Room::displayRoom(Room tempRoom)
 {
-  cout<<"\n Room Number:\t"<<tempRoom.roomNumber;
-  cout<<"\n Type AC/Non-AC(A/N)"<<tempRoom.ac;
-  cout<<"\n Type Comfort(S/N)"<<tempRoom.type;
-  cout<<\"nType size(B/S)"<<tempRoom.stype;
-  cout<<"\n Rent:"<<tempRoom.rent;
-}
-void Room::searchRoom(int rno)
-{
-int i,found=0;
-for(i=0;i<count;i++)
-{
-if(rooms[i].roomNumber==rno)
-{
-found=1;
-break;
-}
-}
-if(found==1)
-{
-cout<<"\nRoom Details :\n";
-if(rooms[i].status==1)
-{
-cout<<"\n  Room is Reserved!";
-}
-else
-{
-cout<<"Room is available!"<<endl;
-cout<<"\n----------------------------------------------";
-}
-displayRoom(rooms[i]);
-getch();
-}
-else
-{
-cout<<"Room not found!"<<endl;
-getch();
-}
-}
-
-void Room::displayRoom(Room tempRoom)
-{
-cout<<left<<setw(40)<<"Room Number"<<tempRoom.roomNumber<<endl;
-cout<<left<<setw(40)<<"AC/Non-AC (A/N)"<<tempRoom.ac<<endl;
-cout<<left<<setw(40)<<"\nComfort SUITE/NON-SUITE (S/N)"<<tempRoom.type<<endl;
-cout<<left<<setw(40)<<"\nSize BIG/SMALL (B/S)"<<tempRoom.stype<<endl;
-cout<<left<<setw(40)<<"\nRent"<<tempRoom.rent<<endl;
+cout<<left<<setw(40)<<"\nRoom Number                   :"<<tempRoom.roomNumber<<endl;
+cout<<left<<setw(40)<<"\nAC/Non-AC (A/N)               :"<<tempRoom.ac<<endl;
+cout<<left<<setw(40)<<"\nComfort SUITE/NON-SUITE (S/N) :"<<tempRoom.type<<endl;
+cout<<left<<setw(40)<<"\nSize BIG/SMALL (B/S)          :"<<tempRoom.stype<<endl;
+cout<<left<<setw(40)<<"\nRent                          :"<<tempRoom.rent<<endl;
+cout<<"\n---------------------------------";
 }   
 
 class HotelMgnt:protected Room
@@ -155,18 +115,45 @@ void HotelMgnt::guestSummaryReport()
 {
   if(count==0)
   {
-    cout<<"\n No guset in Hotel!";
+    cout<<"\n No guest in Hotel!";
   }
   for(int i=0; i<count; I++)
     {
       if(rooms[i].status==1)
       {
-        cout<<left<<setw(20)<<"Customer First Name "<<rooms[i].cust.name<<endl;
-        cout<<left<<setw(20)<<"Room Number "<<rooms[i].roomNumber<<endl;
-        cout<<left<<setw(20)<<"Address (only city) "<<rooms[i].cust.address<<endl;
-        cout<<left<<setw(20)<<"Phone "<<rooms[i].cust.phone<<endl;
-        cout<<"\n---------------------------------------";	
+cout<<"\n-------------------------------------------------";
+cout<<left<<setw(20)<<"\nCustomer First Name  :"<<rooms[i].cust.name<<endl;
+cout<<left<<setw(20)<<"\nRoom Number          :"<<rooms[i].roomNumber<<endl;
+cout<<left<<setw(20)<<"\nAddress (only city)  :"<<rooms[i].cust.address<<endl;
+cout<<left<<setw(20)<<"\nPhone No.            :"<<rooms[i].cust.phone<<endl;
+cout<<left<<setw(20)<<"\nEnter Check-In Date  :"<<rooms[i].cust.from_date<<endl;
+cout<<left<<setw(20)<<"\nEnter Check-Out Date :"<<rooms[i].cust.to_date<<endl;
+cout<<"\n-------------------------------------------------";	
       }
     }
 getch();
+} 
+void HotelMgnt::checkIn()
+{
+int i,found=0,rno;
+
+class Room room;
+cout<<"\n  Enter Room number : ";
+cin>>rno;
+for(i=0;i<count;i++)
+{
+if(rooms[i].roomNumber==rno)
+{
+found=1;
+break;
 }
+}
+if(found==1)
+{
+if(rooms[i].status==1)
+{
+cout<<"\n  Room is already Booked.";
+getch();
+return;
+}
+
